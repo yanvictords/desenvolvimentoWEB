5a247b975a7fd104e4ac5bf2d46afc458f3b634c
Yan <yanvictor_ds@hotmail.com>
2019-02-15 12:59:55 -0200

    Adicionando css

--- front/angular/src/app/main/main.component.ts
+++ front/angular/src/app/main/main.component.ts
@@ -10,6 +10,7 @@ export class MainComponent implements OnInit {
   imagem: string = "https://pro2-bar-s3-cdn-cf2.myportfolio.com/17e02275616cd3dc21a790d8766b3e59/375713d1fdea41438e0dfda4_rw_1200.gif?h=f1a73f9dd54b65023e877291edf808a5"
   nome: string = ''
   email: string = ''
+  senha: string = ''
   cpf: string = ''
   idade: number = null
 
@@ -21,6 +22,7 @@ export class MainComponent implements OnInit {
   onSubmit(){
     console.log('nome', this.nome)
     console.log('email', this.email)
+    console.log('senha', this.senha)
     console.log('cpf', this.cpf)
     console.log('idade', this.idade)
   }
